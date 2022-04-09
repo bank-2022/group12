27bf67d0adb0fc326ca3d26449058e71a6361196
Joonatan Yrjänä <t1yrjo00@students.oamk.fi>
2022-04-09 16:19:41 +0200

    GUI parannuksia

--- pankkiautomaatti/main.cpp
+++ pankkiautomaatti/main.cpp
@@ -5,8 +5,8 @@
 #include <QtWidgets/QApplication>
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
-    QFile styleSheetFile("./SyNet.qss");
+   QApplication app(argc, argv);
+    QFile styleSheetFile("C:/pankkiautomaatti_1/group12/pankkiautomaatti/GUI.qrc");
     styleSheetFile.open(QFile::ReadOnly);
     QString styleSheet = QLatin1String(styleSheetFile.readAll());
     app.setStyleSheet(styleSheet);
--- pankkiautomaatti/pankkiautomaatti.pro
+++ pankkiautomaatti/pankkiautomaatti.pro
@@ -37,6 +37,7 @@ else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
 
 RESOURCES += \
+    GUI.qrc \
     img.qrc \
     resource.qrc
 
