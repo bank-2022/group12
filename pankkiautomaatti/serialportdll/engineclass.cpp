31a3790099dca2d79a264c1d644905509845dbdf
Rikupa <rikupaananen@live.fi>
2022-05-04 10:30:52 +0300

    Muutoksia restapiin ja exeen

--- pankkiautomaatti/serialportdll/engineclass.cpp
+++ pankkiautomaatti/serialportdll/engineclass.cpp
@@ -6,15 +6,14 @@ EngineClass::EngineClass()
 {
     serial = new QSerialPort(this);
     info = new QSerialPortInfo();
-    serial->setPortName("COM4");
+    serial->setPortName("COM5");
     serial->setBaudRate(QSerialPort::Baud9600);
     serial->setDataBits(QSerialPort::Data8);
     serial->setParity(QSerialPort::NoParity);
-    serial->setFlowControl(QSerialPort::NoFlowControl);
+    serial->setFlowControl(QSerialPort::HardwareControl);
     serial->setStopBits(QSerialPort::OneStop);
-//    serial->open(QIODevice::ReadOnly);
+    serial->open(QIODevice::ReadOnly);
     QObject::connect(serial, SIGNAL(readyRead()), this, SLOT(handleSignal()));
-    qDebug()<< "ENGINE: " ;
     info->availablePorts();
     info->standardBaudRates();
     info->portName();
@@ -24,7 +23,6 @@ void EngineClass::handleSignal()
 {
     //Lukee RFID-kortinlukijan avulla RFID-kortin ID numeron
     QString cardID = serial->readAll();
-    qDebug() << "handleSignal: " << cardID;
 
     //Muokkaa luettua cardID:tä ja lähettää muokatun cardID:n eteenpäin
     cardID = cardID.simplified();
